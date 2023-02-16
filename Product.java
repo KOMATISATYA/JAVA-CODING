595bf0a632cc73c0c30f19b68e2957d55b53fafb
KOMATISATYA <125573949+KOMATISATYA@users.noreply.github.com>
2023-02-16 22:26:54 +0530

    Add files via upload

---
+++ Product.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Product {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        int z=x*y;
+        System.out.println("division of two numbers: "+z);
+    }
+}
