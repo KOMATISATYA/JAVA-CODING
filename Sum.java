a43eb47e0d6210498dc854b2c4d7453d46d26b8d
KOMATISATYA <125573949+KOMATISATYA@users.noreply.github.com>
2023-03-04 19:57:33 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Sum {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        int z=x+y;
+        System.out.println("Sum of two numbers: "+z);
+    }
+}
