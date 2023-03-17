00d061aca99fa321c0920254c7cf09347eafb697
KOMATISATYA <125573949+KOMATISATYA@users.noreply.github.com>
2023-03-17 17:30:15 +0530

    throw example

---
+++ exa.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class exa
+{
+  public static void main(String []arga)
+  {
+        throw new ArithmeticException("divisible by Zero");
+  }
+}                                                                      //throw keyword is used create exception object explicitly and handover to the JVM manually.
+                                                                      
